71575e247008d9a6dd338b00bb1659c020807d87
Shinz7777 <78514580+Shinz7777@users.noreply.github.com>
2021-11-22 02:05:22 +0700

    Update dare.js

--- plugins/dare.js
+++ plugins/dare.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch') 
 let handler  = async (m) => {
-  let res = await fetch('https://raw.githubusercontent.com/BochilGaming/Rest-Api/main/src/data/dare.json')
+  let res = await fetch('https://raw.githubusercontent.com/Shinz7777/database/master/kata-kata/dare.json')
   let json = await res.json()
   m.reply(pickRandom(json))
 }
