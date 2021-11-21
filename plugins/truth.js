8659758a5fa03f5ba9b6ef77a1870984e001542b
Shinz7777 <78514580+Shinz7777@users.noreply.github.com>
2021-11-22 02:04:04 +0700

    Update truth.js

--- plugins/truth.js
+++ plugins/truth.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch') 
 let handler  = async (m) => {
-  let res = await fetch('https://raw.githubusercontent.com/BochilGaming/Rest-Api/main/src/data/truth.json')
+  let res = await fetch('https://raw.githubusercontent.com/Shinz7777/database/master/kata-kata/truth.json')
   let json = await res.json()
   m.reply(pickRandom(json))
 }
