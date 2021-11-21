7454cdc4d248923847410a73f1776255d8cff2b2
Shinz7777 <78514580+Shinz7777@users.noreply.github.com>
2021-11-22 01:41:55 +0700

    Update tebakbendera.js

--- plugins/tebakbendera.js
+++ plugins/tebakbendera.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
         throw false
     }
-    if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/BochilTeam/database/master/games/tebakbendera.json'))).json()
+    if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/Shinz7777/database/master/games/tebakbendera.json'))).json()
     let json = src[Math.floor(Math.random() * src.length)]
     if (!json) throw json
     let caption = `
