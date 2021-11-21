eb660c2bf82bb060fc71a50d4c28e1370dbc5d96
Shinz7777 <78514580+Shinz7777@users.noreply.github.com>
2021-11-21 23:45:38 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat [085713964963]
-│ • Gopay [085713964963]
+│ • Indosat [085695157547]
+│ • Gopay [085695157547]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281390658325
+│ > Ingin donasi? Wa.me/6285695157547
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
